2da9e7a6d2f245f91395babefd51e9c99ac843b0
Robin Rohman <122888938+RobinRohman@users.noreply.github.com>
2023-04-14 15:02:16 +0200

    Review

---
+++ Budget-CoolBooks/Controllers/ReviewController.cs
@@ -0,0 +1,29 @@
+﻿using Budget_CoolBooks.Models;
+using Budget_CoolBooks.Services.Authors;
+using Budget_CoolBooks.Services.Books;
+using Budget_CoolBooks.Services.Reviews;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Budget_CoolBooks.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private readonly ReviewServices _ReviewServices;
+
+        public ReviewController(ReviewServices reviewServices)
+        {
+            _ReviewServices = reviewServices;
+            
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await _ReviewServices.GetAllReviews();
+            if (result == null)
+            {
+                return NotFound();
+            }
+            ViewBag.reviewList = result;
+            return View(ViewBag.reviewList);
+        }
+    }
+}
