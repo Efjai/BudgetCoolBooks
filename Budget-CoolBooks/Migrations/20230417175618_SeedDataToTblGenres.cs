b3f0d7ea524e2544e38ad0567b8aaa0b9ff711fc
Hafizullah Rahimi <62449891+HafizullahRahimi@users.noreply.github.com>
2023-04-17 20:09:13 +0200

    Seed data to Tbl Authors, Genres and Books

---
+++ Budget-CoolBooks/Migrations/20230417175618_SeedDataToTblGenres.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Budget_CoolBooks.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDataToTblGenres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Genres(Name, Description, Created)\r\nvalues ('Roman','', GetDate());\r\n\r\nInsert into Genres(Name, Description, Created)\r\nvalues ('Fantasy','', GetDate());\r\n\r\nInsert into Genres(Name, Description, Created)\r\nvalues ('Horror','', GetDate());\r\n\r\nInsert into Genres(Name, Description, Created)\r\nvalues ('Science Fiction','', GetDate());\r\n\r\nInsert into Genres(Name, Description, Created)\r\nvalues ('Thriller','', GetDate());\r\n\r\nInsert into Genres(Name, Description, Created)\r\nvalues ('Biography','', GetDate());\r\n\r\nInsert into Genres(Name, Description, Created)\r\nvalues ('Childrens','', GetDate());");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
