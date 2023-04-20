03a301e2d067ec27d752dd08463b6555c2c65a40
DavidHemb <124883264+DavidHemb@users.noreply.github.com>
2023-04-20 11:44:44 +0200

    Viewmodel

--- Budget-CoolBooks/ViewModels/ReviewcardViewModel.cs
+++ Budget-CoolBooks/ViewModels/ReviewcardViewModel.cs
@@ -6,17 +6,17 @@ namespace Budget_CoolBooks.ViewModels
 {
     public class ReviewcardViewModel
     {
+        public int BookId { get; set; }
         public string ReviewBook { get; set; }
         //public string ReviewAuthor { get; set; }
-        
-
-
-
         public string ReviewUser { get; set; }
         public DateTime ReviewCreated { get; set; }
         public double ReviewRating { get; set; }
         public string ReviewTitle { get; set; }
         public string ReviewText { get; set; }
+        public int Like { get; set; }
+        public int Dislike { get; set; }
+        public int Flag { get; set; }
 
     }
 }
