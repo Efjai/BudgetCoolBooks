d9cf3d6f8c954d0bf972c8fca29f1f16118e3044
Robin Rohman <122888938+RobinRohman@users.noreply.github.com>
2023-04-25 10:57:59 +0200

    Delete Reviews and comment from  user

--- Budget-CoolBooks/Controllers/CommentController.cs
+++ Budget-CoolBooks/Controllers/CommentController.cs
@@ -1,6 +1,7 @@
 ﻿using Budget_CoolBooks.Models;
 using Budget_CoolBooks.Services.Authors;
 using Budget_CoolBooks.Services.Books;
+using Budget_CoolBooks.Services.Comments;
 using Budget_CoolBooks.Services.Reviews;
 using Budget_CoolBooks.Services.UserServices;
 using Budget_CoolBooks.ViewModels;
@@ -18,15 +19,34 @@ namespace Budget_CoolBooks.Controllers
             private readonly ReviewServices _ReviewServices;
             private readonly BookServices _bookServices;
             private readonly UserServices _userServices;
-        public CommentController(ReviewServices reviewServices, BookServices bookServices, UserServices userServices)
+        private readonly CommentServices _commentServices;
+        public CommentController(ReviewServices reviewServices, BookServices bookServices, UserServices userServices, CommentServices commentServices)
         {
             _ReviewServices = reviewServices;
             _bookServices = bookServices;
             _userServices = userServices;
+            _commentServices = commentServices;
         }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]        
+        
+        public async Task<IActionResult> Delete(int commentId)
+        {
+            Comment comment = await _commentServices.GetCommentById(commentId);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            if (!await _commentServices.DeleteComment(comment))
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction("UserComments", "User");
+            
         }
     }
 }
