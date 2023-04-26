a0a9ef5bae9307beef0b7aa4c435beec85e9a4f4
Hafizullah Rahimi <62449891+HafizullahRahimi@users.noreply.github.com>
2023-04-26 20:45:40 +0200

    Do filter day for comment chart

--- Budget-CoolBooks/Controllers/ChartController.cs
+++ Budget-CoolBooks/Controllers/ChartController.cs
@@ -1,11 +1,16 @@
 ﻿using Budget_CoolBooks.Data;
+using Budget_CoolBooks.Models;
+using Budget_CoolBooks.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Budget_CoolBooks.Controllers
 {
     public class ChartController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private static string _commentFilter = "Day"; 
 
         public ChartController(ApplicationDbContext context)
         {
@@ -16,9 +21,42 @@ namespace Budget_CoolBooks.Controllers
             return View();
         }
 
-        public IActionResult Comment()
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Comment(string? filter)
         {
+            if (filter != null)  _commentFilter = filter;
+
+            switch (_commentFilter)
+            {
+                case "Day":
+                    ViewData["commentLabels"] = _context.Comments.GroupBy(x => x.Created.Date)
+                        .Select(grp => grp.Key.ToString("yy-MM-dd")).ToList();
+
+                    ViewData["commentData"] = _context.Comments.GroupBy(x => x.Created.Date)
+                        .Select(grp => grp.Count()).ToList();
+                    break;
+
+                case "Week":
+                    ViewData["commentLabels"] = new List<string>(){ "Art1", "Biography", "Business", "Children's", "Christian" };
+                    ViewData["commentData"] = new List<int>() { 4, 20, 18, 8, 50 };
+                    break;
+
+                case "Author":
+                    break;
+
+                case "Genre":
+                    break;
+
+                default:
+                    break;
+            }
+
+
+            ViewData["commentFilter"] = _commentFilter;
+
             return View();
         }
+
     }
 }
