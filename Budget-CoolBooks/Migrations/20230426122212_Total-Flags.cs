4e2d25df86196e2b698dde4b76f5ff7af5a1ca64
Fredrik Höög <hoogfredrik84@gmail.com>
2023-04-26 14:27:56 +0200

    Total-Flags Migration

---
+++ Budget-CoolBooks/Migrations/20230426122212_Total-Flags.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Budget_CoolBooks.Migrations
+{
+    /// <inheritdoc />
+    public partial class TotalFlags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TotalFlags",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalFlags",
+                table: "AspNetUsers");
+        }
+    }
+}
