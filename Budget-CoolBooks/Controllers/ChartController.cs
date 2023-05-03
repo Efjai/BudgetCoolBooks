dc23f896df74719d74267f51962076334b3f4869
Hafizullah Rahimi <62449891+HafizullahRahimi@users.noreply.github.com>
2023-05-03 17:55:10 +0200

    Update comment chart (add fromDate and endDate)

--- Budget-CoolBooks/Controllers/ChartController.cs
+++ Budget-CoolBooks/Controllers/ChartController.cs
@@ -9,6 +9,8 @@ using System.Globalization;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
 using System.Data;
+using Budget_CoolBooks.Migrations;
+using System.Collections;
 
 namespace Budget_CoolBooks.Controllers
 {
@@ -16,7 +18,7 @@ namespace Budget_CoolBooks.Controllers
     public class ChartController : Controller
     {
         private readonly ApplicationDbContext _context;
-        private static string _commentFilter = "Day"; 
+        private static string _commentFilter = "Day";
 
         public ChartController(ApplicationDbContext context)
         {
@@ -29,44 +31,115 @@ namespace Budget_CoolBooks.Controllers
 
         [HttpGet]
         [HttpPost]
-        public IActionResult Comment(string? filter)
+        public IActionResult Comment(string? filter, DateTime? fromDate, DateTime? endDate)
         {
-            if (filter != null)  _commentFilter = filter;
+            if (filter != null) _commentFilter = filter;
             ViewData["commentFilter"] = _commentFilter;
+
 
             // Comment Filter Controller
             switch (_commentFilter)
             {
                 case "Day":
-                    ViewData["chartLabels"] = _context.Comments.GroupBy(x => x.Created.Date)
-                        .Select(grp => grp.Key.ToString("yy-MM-dd")).ToList();
+                    // All Days
+                    if (fromDate == null)
+                    {
+                        ViewData["chartLabels"] = _context.Comments
+                            .GroupBy(x => x.Created.Date)
+                            .Select(grp => grp.Key.ToString("yy-MM-dd")).ToList();
 
-                    ViewData["chartData"] = _context.Comments.GroupBy(x => x.Created.Date)
-                        .Select(grp => grp.Count()).ToList();
+                        ViewData["chartData"] = _context.Comments
+                            .GroupBy(x => x.Created.Date)
+                            .Select(grp => grp.Count()).ToList();
+                    }
+                    else // Days with fromDate and endDate
+                    {
+                        ViewData["chartLabels"] = _context.Comments
+                            .Where(c => c.Created >= fromDate && c.Created <= endDate)
+                            .GroupBy(x => x.Created.Date)
+                            .Select(grp => grp.Key.ToString("yy-MM-dd")).ToList();
+
+                        ViewData["chartData"] = _context.Comments
+                            .Where(c => c.Created >= fromDate && c.Created <= endDate)
+                            .GroupBy(x => x.Created.Date)
+                            .Select(grp => grp.Count()).ToList();
+                    }
                     break;
 
                 case "Week":
-                    var weeks = _context.Comments.GroupBy(x => x.Created.Date)
-                        .Select(grp => new { label = ISOWeek.GetWeekOfYear(grp.Key), data = grp.Count() }).ToList();
+                    // All Weeks
+                    if (fromDate == null)
+                    {
+                        var weeks = _context.Comments
+                            .GroupBy(x => x.Created.Date)
+                            .Select(grp => new { label = ISOWeek.GetWeekOfYear(grp.Key), data = grp.Count() }).ToList();
 
-                    ViewData["chartLabels"] = weeks.GroupBy(x => x.label).Select(a => a.Key).ToList(); ;
-                    ViewData["chartData"] = weeks.GroupBy(x => x.label).Select(a => a.Sum(b => b.data)).ToList(); 
+                        ViewData["chartLabels"] = weeks.GroupBy(x => x.label).Select(a => a.Key).ToList(); ;
+                        ViewData["chartData"] = weeks.GroupBy(x => x.label).Select(a => a.Sum(b => b.data)).ToList();
+                    }
+                    else // Weeks with fromDate and endDate
+                    {
+                        var weeks = _context.Comments
+                            .Where(c => c.Created >= fromDate && c.Created <= endDate)
+                            .GroupBy(x => x.Created.Date)
+                            .Select(grp => new { label = ISOWeek.GetWeekOfYear(grp.Key), data = grp.Count() }).ToList();
+
+                        ViewData["chartLabels"] = weeks.GroupBy(x => x.label).Select(a => a.Key).ToList(); ;
+                        ViewData["chartData"] = weeks.GroupBy(x => x.label).Select(a => a.Sum(b => b.data)).ToList();
+                    }
+
                     break;
 
                 case "Author":
-                    ViewData["chartLabels"] = _context.CommentsAuthorsFromViews.GroupBy(a => a.Firstname)
-                        .Select(grp => grp.Key).ToList();
+                    // All Authors
+                    if (fromDate == null)
+                    {
+                        ViewData["chartLabels"] = _context.CommentsAuthorsFromViews
+                            .GroupBy(a => a.Firstname)
+                            .Select(grp => grp.Key).ToList();
 
-                    ViewData["chartData"] = _context.CommentsAuthorsFromViews.GroupBy(a => a.Firstname)
-                        .Select(grp => grp.Count()).ToList();
+                        ViewData["chartData"] = _context.CommentsAuthorsFromViews
+                            .GroupBy(a => a.Firstname)
+                            .Select(grp => grp.Count()).ToList();
+                    }
+                    else // Authors with fromDate and endDate
+                    {
+                        ViewData["chartLabels"] = _context.CommentsAuthorsFromViews
+                            .Where(c => c.Created >= fromDate && c.Created <= endDate)
+                            .GroupBy(a => a.Firstname)
+                            .Select(grp => grp.Key).ToList();
+
+                        ViewData["chartData"] = _context.CommentsAuthorsFromViews
+                            .Where(c => c.Created >= fromDate && c.Created <= endDate)
+                            .GroupBy(a => a.Firstname)
+                            .Select(grp => grp.Count()).ToList();
+                    }
                     break;
 
                 case "Genre":
-                    ViewData["chartLabels"] = _context.CommentsGenresFromViews.GroupBy(g => g.Name)
-                        .Select(grp => grp.Key).ToList();
+                    // All Genres
+                    if (fromDate == null)
+                    {
+                        ViewData["chartLabels"] = _context.CommentsGenresFromViews
+                            .GroupBy(g => g.Name)
+                            .Select(grp => grp.Key).ToList();
 
-                    ViewData["chartData"] = _context.CommentsGenresFromViews.GroupBy(g => g.Name)
-                        .Select(grp => grp.Count()).ToList();
+                        ViewData["chartData"] = _context.CommentsGenresFromViews
+                            .GroupBy(g => g.Name)
+                            .Select(grp => grp.Count()).ToList();
+                    }
+                    else // Genres with fromDate and endDate
+                    {
+                        ViewData["chartLabels"] = _context.CommentsGenresFromViews
+                            .Where(c => c.Created >= fromDate && c.Created <= endDate)
+                            .GroupBy(g => g.Name)
+                            .Select(grp => grp.Key).ToList();
+
+                        ViewData["chartData"] = _context.CommentsGenresFromViews
+                            .Where(c => c.Created >= fromDate && c.Created <= endDate)
+                            .GroupBy(g => g.Name)
+                            .Select(grp => grp.Count()).ToList();
+                    }
                     break;
 
             }
